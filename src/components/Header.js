0564481786e5198ff2ddfd72ba5845da0287da16
mohamedagina <mohamedagina001@gmail.com>
2021-04-15 17:03:40 +0200

    feat: style header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from '../images/logo.svg';
 import { Link } from 'react-router-dom';
 import '../stylesheets/Header.scss';
+import illustration from '../images/illustration-working.svg';
 
 const Header = () => {
   return (
@@ -22,6 +23,21 @@ const Header = () => {
           </Link>
         </section>
       </nav>
+      <section className="header-body">
+        <section className="description-container">
+          <span className="heading">More than just shorter links</span>
+          <span className="description">
+            Build your brand`s recognition and get detailed insights on how your
+            links are performing.
+          </span>
+          <Link className="btn pill" to="/get-started">
+            get started
+          </Link>
+        </section>
+        <section className="illustration">
+          <img src={illustration} alt="illustration working"></img>
+        </section>
+      </section>
     </header>
   );
 };
